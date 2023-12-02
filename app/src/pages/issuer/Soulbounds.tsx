0a191e12b4cb6d3333371ec3809e3b80e144b55b
Victor Luna <victorluna22@gmail.com>
2023-12-02 12:47:14 -0300

    Add container class

--- app/src/pages/issuer/Soulbounds.tsx
+++ app/src/pages/issuer/Soulbounds.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import { Tabs } from 'antd'
 import { FileSearchOutlined, FileProtectOutlined } from '@ant-design/icons'
 import { MySoulbounds } from './MySoulbounds/MySoulbounds'
 import { SearchSoulbounds } from './SearchSoulbounds/SearchSoulbounds'
+import { Column } from 'react-display-flex'
 
 const tabs = [
   { icon: FileProtectOutlined, label: 'My Documents', component: MySoulbounds},
@@ -10,21 +10,24 @@ const tabs = [
 ]
 
 export const Soulbounds = () => (
-  <Tabs
-    defaultActiveKey="1"
-    items={tabs.map(({ icon: Icon, label, component: Component }, i) => {
-      const id = String(i + 1);
+  <Column className="container">
+    <Tabs
+      defaultActiveKey="1"
+      size="large"
+      items={tabs.map(({ icon: Icon, label, component: Component }, i) => {
+        const id = String(i + 1);
 
-      return {
-        label: (
-          <span>
-            <Icon />
-            {label}
-          </span>
-        ),
-        key: id,
-        children: <Component />,
-      };
-    })}
-  />
+        return {
+          label: (
+            <span>
+              <Icon />
+              {label}
+            </span>
+          ),
+          key: id,
+          children: <Component />,
+        };
+      })}
+    />
+  </Column>
 )
